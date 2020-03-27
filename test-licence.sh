6b5144bc5d072642dae84b13c4f6be39c1b6ec1e
Salma <salma.eloualydy@gmail.com>
2020-03-27 10:53:05 +0100

    ajout d'un script sh pour le test

---
+++ test-licence.sh
@@ -0,0 +1,6 @@
+# if find . -iname '*.md' -not -path '*/node_modules/*' -not -path '*/tables/.schemas/*' | grep -L '<!-- SPDX-License-Identifier: MPL-2.0 -->' ; 
+if find . -iname '*.md' -not -path '*/node_modules/*' -not -path '*/tables/.schemas/*' -not -path './\.*/*' -exec grep -H -E -o -c '<!-- SPDX-License-Identifier: MPL-2.0 -->' {} \; | grep :0$ ; then 
+    exit 1 
+else
+    echo "tous les fichiers ont une licence"
+fi
