7074837fdfd04daac32e8c919377f1804a4886f6
Salma <salma.eloualydy@gmail.com>
2021-09-17 14:28:41 +0200

    glossary preview cards script

--- .vuepress/glossary-preview.js
+++ .vuepress/glossary-preview.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
+const glob = require("glob")
 const glossary_path = "./glossaire";
-let results = []
-let paths_to_switch = ["./fiches", "./glossaire", ""]
+var results = []
+//let paths_to_switch = ['./fiches', './glossaire', './A_propos', './Boite_aux_lettres', './aller_plus_loin', 
+ //                      './evenements', './formation_snds', './introduction', 'open_data']
+
 
 /**
  * For each glossary file: 
@@ -15,11 +18,11 @@ function getSummary(path) {
         fs.readFile(path + "/"+ filename, function(err, data) {
             if(err) throw err;
             
-            const arr = data.toString().replace(/\r\n/g,'\n').split('\n');
+            let arr = data.toString().replace(/\r\n/g,'\n').split('\n');
             let result = new Object()
             result.path="../glossaire/"+filename
             result.file=filename
-            result.title = arr[0];
+            result.title = arr[0].slice(2);
             if (arr.length < 4) {     
                 result.text = "Aucune définition détaillée n'existe pour l'instant dans le glossaire. Pour contribuer, référrez-vous au guide de contribution.";
             } else {
@@ -30,14 +33,109 @@ function getSummary(path) {
                     result.text = arr[3];
                 } else {
                     result.text = arr[4];
-                }
-                
+                }  
             }
             results.push(result)
-            console.log(result)
         });
         
     });
 }
 
+/**
+ * searches and returns title and 1st line from element that has a matching filename
+ * if filename not matched, returns null
+ * @param {*} filename
+ */
+function getTitleAndTextFromFilename(filename) { 
+    for(let elem of results) {
+        if (elem.file === filename){
+            return elem;
+        }
+    }
+    console.log(filename+" NOT FOUND IN GLOSSAIRE SECTION");
+    return null;
+}
+
+/**
+ * 
+ * 
+ */
+function switchInPaths() { 
+    glob("./!(node_modules|.vuepress|files|contribuer)/**/*.md", function (err, files) {
+        if(err) throw err;
+        files.forEach(filepath => {
+            fs.readFile(filepath, function(err, data) {
+                if(err) throw err;
+
+                let str = data.toString();
+                const array = str.match(/\[[^\[]*\]\(\.\.\/glossaire\/[^\[]*\.md\)/g);
+                if (array){
+                    for(let i of array) {
+                        let arr = i.split('(');
+                        let link_text = arr[0].slice(1,-1);
+                        console.log(link_text)
+                        let link = arr[1].slice(0,-1);
+                        console.log(link)
+                        let filename = link.split('/')[2];
+                        console.log(filename)
+                        link = link.replace(".md",".html");
+                        let result = getTitleAndTextFromFilename(filename);
+                        let markup = "<link-previewer href=\""+link+"\" text=\""+
+                                 link_text+"\" preview-title=\""+result.title+"\" preview-text=\""+result.text+"\" />";
+                        console.log(markup)
+                        //str.replace(i, markup)
+                    }
+                    //console.log(filepath);
+                    //console.log(array);
+                }
+                
+            });
+            
+        });
+  })
+}
+
+function switchInGlossary() { 
+    glob("./glossaire/*.md", function (err, files) {
+        if(err) throw err;
+        files.forEach(filepath => {
+            fs.readFile(filepath, function(err, data) {
+                if(err) throw err;
+
+                let str = data.toString();
+                const array = str.match(/\[[^\[]+\]\([^\/\[]+\.md\)/g);
+                if (array){
+                    for(let i of array) {
+                        let arr = i.split('(');
+                        let link_text = arr[0].slice(1,-1);
+                        //console.log(link_text)
+                        let link = arr[1].slice(0,-1);
+                        //console.log(link)
+                        let filename = link
+                        //console.log(filename)
+                        link = link.replace(".md",".html");
+                        let result = getTitleAndTextFromFilename(filename);
+                        let markup = "<link-previewer href=\""+link+"\" text=\""+
+                                 link_text+"\" preview-title=\""+result.title+"\" preview-text=\""+result.text+"\" />";
+                        //console.log(markup)
+                        str = str.replace(i, markup)
+                        console.log(str)
+                        fs.writeFile("./glossaire/"+filename, str, function (err) { //'utf8', 
+                            if (err) return console.log(err);
+                         });
+                    }
+                    console.log(filepath);
+                    console.log(array);
+                }
+                
+            });
+            
+        });
+  })
+}
+
+
 getSummary(glossary_path);
+switchInPaths();
+switchInGlossary();
+
