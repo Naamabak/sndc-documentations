10ea0112fae0705a1369d9dfa489f0a37653b6ce
Steven Fletcher <steven.fletcher@health-data-hub.fr>
2023-03-09 15:51:36 +0100

    testing

--- .vuepress/indexing.js
+++ .vuepress/indexing.js
@@ -1,8 +1,8 @@
 const getIndexName = function () {
-  const branchName = require("current-git-branch");
+  //   const branchName = require("current-git-branch");
   const siteID = "8b6916da-a2c8-406b-ab0c-b20eb85b8085";
   const build = "netlify";
-  return `${build}_${siteID}_${branchName()}_all`;
+  return `${build}_${siteID}_${process.env.BRANCH}_all`;
 };
 
 exports.getIndexName = getIndexName;
