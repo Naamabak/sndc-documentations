f20f3d92f78c821f307a86a3d5189f34dbb73183
Steven Fletcher <steven.fletcher@health-data-hub.fr>
2023-03-09 15:34:08 +0100

    testing dynamic index name creation

--- .vuepress/indexing.js
+++ .vuepress/indexing.js
@@ -1,5 +1,8 @@
 const getIndexName = function () {
-  return "docSNDS";
+  const branchName = require("current-git-branch");
+  const siteID = "8b6916da-a2c8-406b-ab0c-b20eb85b8085";
+  const build = "netlify";
+  return `${build}_${siteID}_${branchName()}_all`;
 };
 
 exports.getIndexName = getIndexName;
