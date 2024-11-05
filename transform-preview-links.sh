8e4f1c61a283c018a1c79223f4dd36762b2b92d0
nabydiahdh <naby.dia@health-data-hub.fr>
2024-11-05 12:34:27 +0100

    internal_links_for_tests

--- transform-preview-links.sh
+++ transform-preview-links.sh
@@ -10,7 +10,7 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files,contribuer} --include \*.
     else
         seq=$line
     fi
-    output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:\.md):\/" \/>:g' -e 's:\*\*::g' -e 's:README\/::g' -e 's:DCIR\/::g' -e 's:BENEFICIAIRE\/::g' -e 's:DCIR_DCIRS\/::g' -e 's:PMSI\/PMSI MCO\/::g' -e 's:PMSI\/PMSI%20MCO\/::g' -e 's:PMSI\/PMSI HAD\/::g' -e 's:PMSI\/PMSI SSR\/::g' -e 's:PMSI\/PMSI RIM-P\/::g' -e 's:DAMIR\/::g' -e 's:DCIRS\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g' -e 's:EGB\/::g' ) 
+    output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:\.md):\/" \/>:g' -e 's:\*\*::g' -e 's:README\/::g' -e 's:DCIR\/::g' -e 's:REFERENTIELS\/::g' -e 's:PMSI MCO\/::g' -e 's:PMSI%20MCO\/::g' -e 's:PMSI HAD\/::g' -e 's:PMSI SSR\/::g' -e 's:PMSI RIP\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g' ) 
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
     #sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
@@ -22,7 +22,7 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files} --include \*.md '\[[^\/]
     read -a parts <<< "$line"
     seq=${parts[1]}
     file=${parts[0]}
-    output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:):" \/>:g' -e 's:DCIR\/::g' -e 's:BENEFICIAIRE\/::g' -e 's:DCIR_DCIRS\/::g' -e 's:PMSI\/PMSI MCO\/::g' -e 's:PMSI\/PMSI%20MCO\/::g' -e 's:PMSI\/PMSI HAD\/::g' -e 's:PMSI\/PMSI SSR\/::g' -e 's:PMSI\/PMSI RIM-P\/::g' -e 's:DAMIR\/::g' -e 's:DCIRS\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g' -e 's:EGB\/::g' ) 
+    output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:):" \/>:g' -e 's:DCIR\/::g' -e 's:REFERENTIELS\/::g' -e 's:PMSI MCO\/::g' -e 's:PMSI%20MCO\/::g' -e 's:PMSI HAD\/::g' -e 's:PMSI SSR\/::g' -e 's:PMSI RIP\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g'  ) 
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
     #sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
